c1cb7a0f778eadb770b46d5a27d10519c8b520a2
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-30 15:21:08 -0700

    Add tests with the WordDisplay and IsCorrectGuess methods

--- Hangman.Tests/ModelTests/WordTests.cs
+++ Hangman.Tests/ModelTests/WordTests.cs
@@ -60,5 +60,80 @@ namespace Hangman.Tests
       Assert.AreEqual(result, 1);
       
     }
+    [TestMethod]
+    public void Word_DetermineValidGuess_True()
+    {
+      //arrange
+      Word testWord = new Word();
+      Guess guess = new Guess("h");
+      
+      //act
+      bool result = testWord.IsCorrectGuess(guess);
+      
+      //assert
+      Assert.AreEqual(result, true);
+    }
+    [TestMethod]
+    public void Word_DetermineInvalidGuess_True()
+    {
+      //arrange
+      Word testWord = new Word();
+      Word testWord2 = new Word();
+      Guess guess = new Guess("h");
+      
+      //act
+      bool result = testWord2.IsCorrectGuess(guess);
+      
+      //assert
+      Assert.AreEqual(result, false);
+    }
+    [TestMethod]
+    public void Word_AddsGuessToList_True()
+    {
+      //arrange
+      Word testWord = new Word();
+      Guess guess = new Guess("h");
+      testWord.Guesses.Add(guess);
+      testWord.IsCorrectGuess(guess);
+      //act
+      bool result = testWord.Guesses[0].InWord;
+      
+      //assert
+      Assert.AreEqual(true, result);
+    }
+    [TestMethod]
+    public void WordDisplay_ReturnsGuessedCharacters_h()
+    {
+      //arrange
+      Word testWord = new Word();
+      Guess guess = new Guess("h");
+      testWord.Guesses.Add(guess);
+      string expected = "h----";
+      //act
+      string result = testWord.WordDisplay();
+
+      //assert
+      Assert.AreEqual(expected, result);
+    }
+    [TestMethod]
+    public void WordDisplay_ReturnsGuessedCharacters_hello()
+    {
+      //arrange
+      Word testWord = new Word();
+      Guess guess = new Guess("h");
+      testWord.Guesses.Add(guess);
+      Guess guess2 = new Guess("e");
+      testWord.Guesses.Add(guess2);
+      Guess guess3 = new Guess("l");
+      testWord.Guesses.Add(guess3);
+      Guess guess4 = new Guess("o");
+      testWord.Guesses.Add(guess4);
+      string expected = "hello";
+      //act
+      string result = testWord.WordDisplay();
+
+      //assert
+      Assert.AreEqual(expected, result);
+    }
   }
 }
--- Hangman/Models/Word.cs
+++ Hangman/Models/Word.cs
@@ -1,6 +1,7 @@
 // multiple collections in a list (type of guess) Player1 guesses Words
 //   word constructor - List _allWords - won/lost bool prop
 using System.Collections.Generic;
+using System;
 
 namespace Hangman.Models
 {
@@ -10,38 +11,86 @@ namespace Hangman.Models
     public bool Survived {get;set;}
     public int Attempts {get;set;}
     public int Id {get;}
-    public List<Guess> Guesses = new List<Guess>(){
-    };
+    public List<Guess> Guesses = new List<Guess>();
+    //do we actually want a list of characters? 
+    //are we benefitting from having a list of Guess objects in some other way?
+    //MVC perspective: displaying one specific guess with its properties (like todolist item with description/pri)
     //cat - list of item 
 
     private static List<Word> _usedWords = new List<Word>();
     private static List<string> _potentialWords = new List<string>(4){"hello","world","foo","bar"};
     //constructor - pull a word from the list
-  public Word()
-  {
-    _usedWords.Add(this);
-    Id = _usedWords.Count;
-    Letters = _potentialWords[Id - 1];
-    Attempts = Letters.Length + 2;
-    Survived = true;
-  }
+    public Word()
+    {
+      _usedWords.Add(this);
+      Id = _usedWords.Count;
+      Letters = _potentialWords[Id - 1];
+      Attempts = Letters.Length + 2;
+      Survived = true;
+    }
 
-  public static void ClearAll()
-  {
-    _usedWords.Clear();
-  }
+    public static void ClearAll()
+    {
+      _usedWords.Clear();
+    }
+
+    // public List<Guess> GetAll()
+    // {
+    //   return Guesses;
+    // }
+
+    public bool IsCorrectGuess(Guess currentGuess) //a method should only do one thing
+    {
+      // Guess guessedLetter = new Guess(letter); //create it in a controller?
+      if (Letters.Contains(currentGuess.Letter))
+      {
+        currentGuess.InWord = true;
+        return true;
+      }
+      else
+      {
+        currentGuess.InWord = false;
+        return false;
+      }
+    }
+
+    public string WordDisplay()
+    {
+      string gameWord = Letters;
+      // string[] letterArray = gameWord.Split("");
+      // Console.WriteLine($"Guesses: {letterArray}");
+      string displayString = "";
+
+      //look at each letter in the correct word 
+      //if the user has guessed it, add on display string at that index, else add -
+
+      for(int index = 0; index < gameWord.Length; index++)
+      {
+        Console.WriteLine($"letter: {gameWord[index]}");
+        
+        int startingLength = displayString.Length;
+        //easier to work with a string!
+        for(int i = 0; i < Guesses.Count; i++)
+        {
+          if (Guesses[i].Letter == gameWord[index].ToString())
+          {
+            displayString += gameWord[index];
+          }
+        }
+        //if the lenght is the same as it used to be
+        if (displayString.Length == startingLength)
+        {
+          displayString += "-";
+        }
+
+      }
+        return displayString;
+    }
     // logic for repeat letters or not a problem?
 
-    // public bool IsCorrectGuess()
-    // {
-
-    // }
-    //getall
-
-    //clearall
 
     //find
 
     //get
-  }
-}
+  } // class
+} // namespace
