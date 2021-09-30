4b86477a1abd3bd83a0667a6d86704e05a817af2
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-30 11:21:25 -0700

    Add Guess constructor with tests

--- Hangman/Controllers/HomeController.cs
+++ Hangman/Controllers/HomeController.cs
@@ -10,3 +10,40 @@ namespace Hangman.Controllers
     }
   }
 }
+/*
+guesses property - int
+guessed letters property - string
+
+guess() - check duplicates
+
+thing - guess myGuess.Character = 'r' .GuessNumber .IsInWord
+collection - _guesse
+multiple collections in a list (type of guess) Player1 guesses Words
+  word constructor - List _allWords - won/lost bool prop
+  
+methods:
+guess()
+IsInWord()
+
+thing = hangman Game
+
+game {
+  turns:
+  word:
+  player 1 {[]}
+  player
+}
+
+
+b _ b b _ _
+*/
+
+// @{
+//   @if (guess right)
+//   {
+//   layout=_RightLayout
+//   }
+//   else {
+//     layout=_WrongLayout
+//   }
+// }
